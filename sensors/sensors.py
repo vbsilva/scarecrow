dbf607a1a99fbbc582378cb657928e34808edc74
Vinicius Bezerra <vbas@cin.ufpe.br>
2017-08-28 03:48:12 -0300

    added ldr

--- sensors/sensors.py
+++ sensors/sensors.py
@@ -2,27 +2,37 @@ import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
  
-# Define o tipo de sensor
 dht = Adafruit_DHT.DHT11
  
 GPIO.setmode(GPIO.BCM)
  
-# Define a GPIO conectada ao pino de dados do sensor
 dht_pin = 2
+ldr_pin = 3
+
+def rc_time(ldr_pin):
+  count = 0
+
+  GPIO.setup(ldr_pin, GPIO.OUT)
+  GPIO.output(ldr_pin, GPIO.LOW)
+  time.sleep(0.1)
+
+  GPIO.setup(ldr_pin, GPIO.IN)
+
+  while(GPIO.input(ldr_pin) == GPIO.LOW):
+    count += 1
+
+  return count
  
-# Informacoes iniciais
-print ("*** Lendo os valores de temperatura e umidade");
  
 while(1):
-   # Efetua a leitura do sensor
-   umid, temp = Adafruit_DHT.read_retry(dht, dht_pin);
-   # Caso leitura esteja ok, mostra os valores na tela
-   if umid is not None and temp is not None:
-     print("temperature: " + str(temp),)
-     print("humidity: " + str(umid))
-     #print ("Temperatura = {0:0.1f}  Umidade = {1:0.1f}n").format(temp, umid);
-     print ("Aguarda 5 segundos para efetuar nova leitura...n");
-     time.sleep(5)
-   else:
-     # Mensagem de erro de comunicacao com o sensor
-     print("Falha ao ler dados do DHT11 !!!")
+
+  print (rc_time(ldr_pin))
+
+  umid, temp = Adafruit_DHT.read_retry(dht, dht_pin);
+  if umid is not None and temp is not None:
+    print("temperature: " + str(temp),)
+    print("humidity: " + str(umid))
+    print ("Aguarda 5 segundos para efetuar nova leitura...");
+    time.sleep(2)
+  else:
+    print("Falha ao ler dados do DHT11 !!!")
